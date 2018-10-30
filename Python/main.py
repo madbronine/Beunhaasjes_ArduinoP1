5384d487443f43fdf9fcd9efc91d665fa5f0b3b5
Jeroen <jeroenwesters@hotmail.com>
2018-10-30 21:51:27 +0100

    ADD Comport detection

--- Python/main.py
+++ Python/main.py
@@ -1,16 +1,20 @@
-import controllers
+import controller
 import time
 
 def main():
     # program loop
     while(True):
-        print('program loop')
+        print('=============')
 
-        #
-        controllers.find_comports()
+        #Refresh the port list
+        controller.run()
+        print('Current com devices: {0}'.format(controller.get_modules()))
+
+        print('===========')
+        print('')
+        print('')
 
         time.sleep(2) #This and import time should be removed (when main program loop is added + timed)
-
 
 #####################################################
 
