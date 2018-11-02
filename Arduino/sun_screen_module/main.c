86d068c1896f6f123d2aa04b6457a8b75a17fa04
Jeroen <jeroenwesters@hotmail.com>
2018-11-02 11:37:22 +0100

    [Init] Init C, Updated gitignore

---
+++ Arduino/sun_screen_module/main.c
@@ -0,0 +1,18 @@
+/*
+ * sun_screen_module.c
+ *
+ * Created: 2-11-2018 11:29:19
+ * Author : jeroe
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
