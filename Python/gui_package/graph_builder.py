276808e0162e3e46cc50b655c2ea03ac0051f2f4
Jeroen <jeroenwesters@hotmail.com>
2018-11-14 16:39:57 +0100

    [Add] comments

--- Python/gui_package/graph_builder.py
+++ Python/gui_package/graph_builder.py
@@ -3,6 +3,7 @@ from tkinter import *
 import random
 import time
 
+# Class to store x and y position with its index (time frame)
 class Point:
     def __init__(self, x, y, index):
         self.x = x
@@ -18,24 +19,31 @@ class Point:
     def get_index(self):
         return self.index
 
+# Class to hold the graph
 class Graph():
+    # Initialize with a root frame, min value and max value
     def __init__(self, root, min_value, max_value):
         self.root=root
         self.entry = tk.Entry(root)
         stvar=tk.StringVar()
         stvar.set("one")
+
+        #Offset from bottom and sides
         self.offset = 50 #offset from bottom
 
         self.min_value = min_value
         self.max_value = max_value
 
 
+        #Create canvas
         self.canvas=tk.Canvas(root, width=550, height=450, background='white')
         self.canvas.grid(row=0,column=1)
 
+        #reate frame
         frame = Frame(self.root)
         frame.grid(row=0,column=0, sticky="n")
 
+        #Update canvas to update values from canvas itself
         root.update()
 
         self.create_grid()
@@ -49,7 +57,7 @@ class Graph():
         #self.create_graph_line(0, 20, 10, 18)
 
 
-
+    #Creates grid based on size and parameters
     def create_grid(self):
         canv = self.canvas
 
@@ -77,7 +85,7 @@ class Graph():
         self.box_size = amount_box_x
         self.step = step_size
 
-        # amount_box space
+        # create vertical lines
         for x in range(0, amount):
             start_xPos = self.offset
             end_xPos = canv.winfo_width() - self.offset
@@ -89,6 +97,7 @@ class Graph():
         self.amount_boxy = amount_box_y
         max_size = self.min_value + step_size * (amount-1)
 
+        # create horizontal lines
         for y in range(0, amount):
             start_yPos = self.offset
             end_xPos = canv.winfo_width()
@@ -101,12 +110,13 @@ class Graph():
             if value == 0:
                 self.zero_height = start_yPos +(y * amount_box_y) - self.offset;
 
-
+        #Create bottom and side lines
         canv.create_line(self.offset, 0, self.offset, start_yPos +(amount * amount_box_y) - self.offset, width=2, tags="field", fill="black")
         canv.create_line(self.offset, start_yPos +(amount * amount_box_y) - self.offset, start_xPos + (amount * amount_box_x), start_yPos +(amount * amount_box_y) - self.offset, width=2, tags="field", fill="black")
 
         #print(':', self.zero_height)
 
+    #Fill graph with data
     def create_data(self):
         #print(':', self.zero_height)
 
@@ -119,6 +129,7 @@ class Graph():
         return data
 
 
+    #Draw graphic
     def create_graph(self):
         canv = self.canvas
         # loop through data!
@@ -141,8 +152,7 @@ class Graph():
                 old_pos = point
                 first = False
 
-
-        # Draw to field
+    # Draw line to canvas
     def create_graph_line(self, old_x, old_y, new_x, new_y, index):
         canv = self.canvas
 
@@ -151,10 +161,7 @@ class Graph():
 
         canv.create_text(new_x + self.offset, canv.winfo_height() - (self.offset/2),fill="darkblue",font="Times 7", text=index, tags="data")
 
-
-
-
-
+    # Add data to the graph
     def add_data(self, value):
         self.data.pop(0) # Remove first item
         #yPos = (value / self.step)
@@ -166,10 +173,6 @@ class Graph():
         self.data.append(p)
         self.canvas.delete('data')
         self.create_graph()
-
-#self.box_size = amount_box_x
-#self.step = step_size
-#self.zero_height
 
 
 
