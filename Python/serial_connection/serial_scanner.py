320a7b65493187f38db84fada52d977711b8d4c6
Jeroen <jeroenwesters@hotmail.com>
2018-11-14 22:24:17 +0100

    [Fix] serial_scanner pep8

--- Python/serial_connection/serial_scanner.py
+++ Python/serial_connection/serial_scanner.py
@@ -1,9 +1,12 @@
 # © Jeroen - 31-10-2018
-# This file handles: scanning for ports connected, check existing connections and removing old ports from a dictionary
+# This file handles: scanning for ports connected,
+# check existing connections and removing old ports from a dictionary
 
 from serial.tools import list_ports
 
 # Returns a list with ports that are currently attached
+
+
 def get_new_ports(existing_ports):
     new_ports = find_available_ports()
 
@@ -14,18 +17,22 @@ def get_new_ports(existing_ports):
     return new_ports
 
 # Handles module searching
+
+
 def find_available_ports():
     # Get all comports connected
-    ports = list_ports.comports()   #list of all ports connected
-    result = []                     #list to store comports
+    ports = list_ports.comports()  # list of all ports connected
+    result = []  # list to store comports
 
     for port in ports:
-        port_found = port[0]        #port that has been found
-        result.append(port_found)   #add port to the list
+        port_found = port[0]  # port that has been found
+        result.append(port_found)  # add port to the list
 
     return result
 
 # Check if port is still connected
+
+
 def check_connection(comport):
     # Get all connected com ports
     all_ports = find_available_ports()
